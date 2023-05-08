6f582327898e7a49f49cb1a7f91a6ec5aa0d862b
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-05-08 13:56:35 -0400

    iniitial commit

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,10 +1,17 @@
-import cloudinary from 'cloudinary'
+import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "gallerysite",
+  api_key: "192567674179358",
+  api_secret: "L3RaodQdozlX9gMTcn4zwP0v1kM",
   secure: true,
-})
+});
 
-export default cloudinary
+export default cloudinary;
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+//   secure: true,
+// })
