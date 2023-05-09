d6501a7242a8f8826e8b94f67baa742b3133480d
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-05-08 23:51:29 -0400

    svg is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 import Bridge from "../components/Icons/Bridge";
+import Station from "../components/Icons/Station";
 import Logo from "../components/Icons/Logo";
 import Modal from "../components/Modal";
 import cloudinary from "../utils/cloudinary";
@@ -47,13 +48,13 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
           <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
             <div className="absolute inset-0 flex items-center justify-center opacity-20">
               <span className="flex max-h-full max-w-full items-center justify-center">
-                <Bridge />
+                <Station />
               </span>
               <span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
-            <Logo />
+            {/*<Logo />*/}
             <h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+              Streets 40 years after ...
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
               New Photo Gallery App
