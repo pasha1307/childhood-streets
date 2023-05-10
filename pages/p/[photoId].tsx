dc71f7f48fce0f093c4d0a7534b198a4efd6db04
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-05-10 19:55:30 -0400

    test commit

--- pages/p/[photoId].tsx
+++ pages/p/[photoId].tsx
@@ -17,7 +17,7 @@ const Home: NextPage = ({ currentPhoto }: { currentPhoto: ImageProps }) => {
   return (
     <>
       <Head>
-        <title>Childhood Streets Now</title>
+        <title>Childhood Streets - Present</title>
         <meta property="og:image" content={currentPhotoUrl} />
         <meta name="twitter:image" content={currentPhotoUrl} />
       </Head>
