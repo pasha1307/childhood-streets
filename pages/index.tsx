a17c8526b879fdcc913d120cf805dd585619bb8c
pasha1307 <pavelkrotov@Pavels-MacBook-Pro-2.local>
2025-05-06 20:33:34 -0400

    April photos added

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
   return (
     <>
       <Head>
-        <title>Recent and Distant Memories</title>
+        <title>Recent and Distant Memories.</title>
         <meta property="og:image" content="Views of Sysktyvkar Town and Sysktyvakr Area" />
         <meta name="twitter:image" content="Peter Banasyak" />
       </Head>
