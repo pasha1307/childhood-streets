0856f275a19d19ff4e2b4f8ebe226b9e3f47a782
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-01-14 17:51:00 -0500

    title is adjusted

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 	return (
 		<>
 			<Head>
-				<title>Memories and Realities</title>
+				<title>Close and Distant Memories</title>
 				<meta property="og:image" content="Sysktyvkar City and Around" />
 				<meta name="twitter:image" content="Peter Banasyak" />
 			</Head>
