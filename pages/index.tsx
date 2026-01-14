d80000a88413d30d73de52f2b62036f421b87fda
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-01-14 17:41:29 -0500

    new photoes are added

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,120 +12,124 @@ import type { ImageProps } from "../utils/types";
 import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
 
 const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
-  const router = useRouter();
-  const { photoId } = router.query;
-  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
+	const router = useRouter();
+	const { photoId } = router.query;
+	const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
 
-  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
+	const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
 
-  useEffect(() => {
-    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync with back button
-    if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
-      setLastViewedPhoto(null);
-    }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+	useEffect(() => {
+		// This effect keeps track of the last viewed photo in the modal to keep the index page in sync with back button
+		if (lastViewedPhoto && !photoId) {
+			lastViewedPhotoRef.current.scrollIntoView({ block: "center" });
+			setLastViewedPhoto(null);
+		}
+	}, [photoId, lastViewedPhoto, setLastViewedPhoto]);
 
-  return (
-    <>
-      <Head>
-        <title>Recent and Distant Memories.</title>
-        <meta property="og:image" content="Views of Sysktyvkar Town and Sysktyvakr Area" />
-        <meta name="twitter:image" content="Peter Banasyak" />
-      </Head>
-      <main className="mx-auto max-w-[1960px] p-4">
-        {photoId && (
-          <Modal
-            images={images}
-            onClose={() => {
-              setLastViewedPhoto(photoId);
-            }}
-          />
-        )}
-        <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-          <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
-            <div className="absolute inset-0 flex items-center justify-center opacity-20">
-              <span className="flex max-h-full max-w-full items-center justify-center">
-                <Station />
-              </span>
-              <span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
-            </div>
-            {/*<Logo />*/}
-            <h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
-              current views of neighborhoods and countryside ...
-            </h1>
-            <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Bur Ryt, Dona Yortyas, .., Mian Kar, Dona Kar ...Lebalei Samolyotyasen!
-            </p>
-          </div>
-          {images.map(({ id, public_id, format, blurDataUrl }) => (
-            <Link
-              key={id}
-              href={`/?photoId=${id}`}
-              as={`/p/${id}`}
-              ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-              shallow
-              className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-            >
-              <Image
-                alt="est"
-                className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                style={{ transform: "translate3d(0, 0, 0)" }}
-                placeholder="blur"
-                blurDataURL={blurDataUrl}
-                src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                width={720}
-                height={480}
-                sizes="(max-width: 640px) 100vw,
+	return (
+		<>
+			<Head>
+				<title>Close and Distant Memories.</title>
+				<meta
+					property="og:image"
+					content="Views of Sysktyvkar Town and Sysktyvakr Area"
+				/>
+				<meta name="twitter:image" content="Peter Banasyak" />
+			</Head>
+			<main className="mx-auto max-w-[1960px] p-4">
+				{photoId && (
+					<Modal
+						images={images}
+						onClose={() => {
+							setLastViewedPhoto(photoId);
+						}}
+					/>
+				)}
+				<div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
+					<div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+						<div className="absolute inset-0 flex items-center justify-center opacity-20">
+							<span className="flex max-h-full max-w-full items-center justify-center">
+								<Station />
+							</span>
+							<span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
+						</div>
+						{/*<Logo />*/}
+						<h1 className="mb-4 mt-8 text-base font-bold uppercase tracking-widest">
+							current views of neighborhoods and countryside ...
+						</h1>
+						<p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
+							Bur Ryt, Dona Yortyas, .., Mian Kar, Dona Kar ...Lebalei
+							Samolyotyasen!
+						</p>
+					</div>
+					{images.map(({ id, public_id, format, blurDataUrl }) => (
+						<Link
+							key={id}
+							href={`/?photoId=${id}`}
+							as={`/p/${id}`}
+							ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
+							shallow
+							className="after:content group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+						>
+							<Image
+								alt="est"
+								className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+								style={{ transform: "translate3d(0, 0, 0)" }}
+								placeholder="blur"
+								blurDataURL={blurDataUrl}
+								src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+								width={720}
+								height={480}
+								sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-              />
-            </Link>
-          ))}
-        </div>
-      </main>
-      <footer className="p-6 text-center text-white/80 sm:p-12">
-        <small>@PAB-PPK-ABK</small>
-      </footer>
-    </>
-  );
+							/>
+						</Link>
+					))}
+				</div>
+			</main>
+			<footer className="p-6 text-center text-white/80 sm:p-12">
+				<small>@PAB-PPK-ABK</small>
+			</footer>
+		</>
+	);
 };
 
 export default Home;
 
 export async function getStaticProps() {
-  const results = await cloudinary.v2.search
-    .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
-    .sort_by("public_id", "desc")
-    .max_results(400)
-    .execute();
-  let reducedResults: ImageProps[] = [];
+	const results = await cloudinary.v2.search
+		.expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+		.sort_by("public_id", "desc")
+		.max_results(400)
+		.execute();
+	let reducedResults: ImageProps[] = [];
 
-  let i = 0;
-  for (let result of results.resources) {
-    reducedResults.push({
-      id: i,
-      height: result.height,
-      width: result.width,
-      public_id: result.public_id,
-      format: result.format,
-    });
-    i++;
-  }
+	let i = 0;
+	for (let result of results.resources) {
+		reducedResults.push({
+			id: i,
+			height: result.height,
+			width: result.width,
+			public_id: result.public_id,
+			format: result.format,
+		});
+		i++;
+	}
 
-  const blurImagePromises = results.resources.map((image: ImageProps) => {
-    return getBase64ImageUrl(image);
-  });
-  const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+	const blurImagePromises = results.resources.map((image: ImageProps) => {
+		return getBase64ImageUrl(image);
+	});
+	const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
 
-  for (let i = 0; i < reducedResults.length; i++) {
-    reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
-  }
+	for (let i = 0; i < reducedResults.length; i++) {
+		reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+	}
 
-  return {
-    props: {
-      images: reducedResults,
-    },
-  };
+	return {
+		props: {
+			images: reducedResults,
+		},
+	};
 }
