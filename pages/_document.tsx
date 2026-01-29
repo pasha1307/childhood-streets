589609e1fbb3b577b1d1d1658c131819b94980f0
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-01-29 17:27:03 -0500

    change in meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,39 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="See photos of sites in and out of former hometown Syktyvkar"
-          />
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="icon" href="/favicon.ico" />
+					<meta
+						name="description"
+						content="See photos of sites in and out of former hometown Syktyvkar"
+					/>
 
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
-          <meta
-            property="og:description"
-            content="Updated pictures from Komi old and current sites in progress."
-          />
-          <meta property="og:title" content="Childhood Sites Today" />
-          <meta name="twitter:card" content="summary_site_image" />
-          <meta name="twitter:title" content="Childhood Sites Years After" />
-          <meta
-            name="twitter:description"
-            content="In progress photography of sites around Syktyvkar."
-          />
-        </Head>
-        <body className="bg-black antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+					<meta
+						property="og:site_name"
+						content="Photo Gallery Sites In Syktyvkar"
+					/>
+					<meta
+						property="og:description"
+						content="Updated pictures from Komi old and current sites in progress."
+					/>
+					<meta property="og:title" content="Childhood Sites Today" />
+					<meta name="twitter:card" content="summary_site_image" />
+					<meta name="twitter:title" content="Childhood Sites Years After" />
+					<meta
+						name="twitter:description"
+						content="In progress photography of sites around Syktyvkar."
+					/>
+				</Head>
+				<body className="bg-black antialiased">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
