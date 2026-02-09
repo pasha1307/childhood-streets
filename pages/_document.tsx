94fe462c3a03e910bec222627ae8fe6986601ddf
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-02-08 20:26:11 -0500

    Nadya and Vera photos were added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,12 +19,15 @@ class MyDocument extends Document {
 						property="og:description"
 						content="Updated pictures from Komi old and current sites in progress."
 					/>
-					<meta property="og:title" content="Childhood Sites Today" />
+					<meta
+						property="og:title"
+						content="Fifty Years Of Graduation From High School No 12 In Syktyvkar"
+					/>
 					<meta name="twitter:card" content="summary_site_image" />
 					<meta name="twitter:title" content="Childhood Sites Years After" />
 					<meta
 						name="twitter:description"
-						content="In progress photography of sites around Syktyvkar."
+						content="IFifty Years Of Graduation From High School No 12 In Syktyvkar."
 					/>
 				</Head>
 				<body className="bg-black antialiased">
