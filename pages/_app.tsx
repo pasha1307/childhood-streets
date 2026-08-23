c75ac81e0cd5f8a2cc4f8927cd4aad8e6609f8c0
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-08-23 10:43:30 -0400

    comments were removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-// @ts-expect-error -- Next.js handles global CSS side-effect imports
 import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
