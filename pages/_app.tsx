fc2352fec2f652934243ff75fcb43447f42b9f47
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-08-23 10:41:07 -0400

    analytics was added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import type { AppProps } from 'next/app'
-import '../styles/index.css'
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+// @ts-expect-error -- Next.js handles global CSS side-effect imports
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<SpeedInsights />
+		</>
+	);
 }
