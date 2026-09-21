98fdf0d485378a05368a0fdfe8d5fea45c89c142
pasha1307 <pavelkrotov@Mac.hsd1.ma.comcast.net>
2026-09-21 10:23:36 -0400

    one more photo is added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,7 +30,7 @@ class MyDocument extends Document {
 					/>
 					<meta
 						name="twitter:description"
-						content="Fifty Years After Graduation. Views of Syktyvkar and Around. In progress."
+						content="Fifty Years After Graduation. Views of Syktyvkar and Around. School No.12. In progress."
 					/>
 				</Head>
 				<body className="bg-black antialiased">
